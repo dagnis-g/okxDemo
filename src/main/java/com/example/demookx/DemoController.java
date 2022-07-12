d46a33081e8a2716e2f5ad9f756c4781c06dc953
DagnisGulbis <dagnisgulbis1@gmail.com>
2022-07-12 20:17:16 +0300

    added demo header...still "msg":"Invalid Sign","code":"50113"

--- src/main/java/com/example/demookx/DemoController.java
+++ src/main/java/com/example/demookx/DemoController.java
@@ -21,11 +21,13 @@ import java.util.Base64;
 @Slf4j
 public class DemoController {
 
+
+    String apiKey = Secrets.API_KEY;
+    String secretKey = Secrets.SECRET_KEY;
+    String passphrase = Secrets.PASSPRHASE;
+
     @GetMapping("/verify")
     public String verify() throws IOException {
-        String apiKey = "";
-        String secretKey = "";
-        String passphrase = "";
 
         String instant = Instant.now().toString();
         final String timestamp = instant.substring(0, instant.length() - 7) + "Z";
@@ -34,19 +36,21 @@ public class DemoController {
 
         String hmacUri = timestamp + "GET" + uriPath;
         String hmac = new HmacUtils(HmacAlgorithms.HMAC_SHA_256, secretKey).hmacHex(hmacUri);
+        log.warn(hmac);
         var okAccessSign = Base64.getEncoder().encodeToString(hmac.getBytes());
+        log.warn(okAccessSign);
 
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet request = new HttpGet("https://www.okx.com/" + uriPath);
+        HttpGet request = new HttpGet("https://www.okex.com/" + uriPath);
 
+        request.addHeader("Content-type", "application/json");
         request.addHeader("OK-ACCESS-KEY", apiKey);
-        request.addHeader("OK-ACCESS-SIGN", "sign=" + okAccessSign);
-//        request.addHeader("OK-ACCESS-SIGN", okAccessSign);
-        request.addHeader("OK-ACCESS-TIMESTAMP", timestamp);
+        request.addHeader("OK-ACCESS-SIGN", okAccessSign);
         request.addHeader("OK-ACCESS-PASSPHRASE", passphrase);
-//        request.addHeader("Content-type", "application/json");
+        request.addHeader("OK-ACCESS-TIMESTAMP", timestamp);
+        request.addHeader("x-simulated-trading", "1");
 
-        log.info(Arrays.toString(request.getAllHeaders()));
+        log.warn(Arrays.toString(request.getAllHeaders()));
 
         CloseableHttpResponse response = httpClient.execute(request);
         HttpEntity entity = response.getEntity();
